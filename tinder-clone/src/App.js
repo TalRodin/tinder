61406d5ac75890f925c252e042d980038416811b
Alyona <alyona.rodin@gmail.com>
2020-09-25 17:47:20 -0400

    first commit

--- tinder-clone/src/App.js
+++ tinder-clone/src/App.js
@@ -3,13 +3,25 @@ import './App.css';
 import Header from './Header'
 import TinderCards from './TinderCards'
 import SwipeButtons from './SwipeButtons'
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
 
 function App() {
   return (
     <div className="app">
       <Header />
-      <TinderCards/>
-      <SwipeButtons/>
+      <Router>
+        <Switch>
+           <Route path='/chat'>
+              <h1>chat</h1>
+           </Route>
+           <Route path='/'>
+              <TinderCards/>
+              <SwipeButtons/>
+          </Route>
+          
+        </Switch>
+      </Router>
+      
     </div>
   );
 }
